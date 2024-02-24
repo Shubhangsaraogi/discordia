f315c942bb4c26629724f72dc210e34e33deae26
Shubhang <s.saraogi1902@gmail.com>
2024-02-25 01:49:39 +0530

    typos fixed

--- types.ts
+++ types.ts
@@ -3,7 +3,7 @@ import { NextApiResponse } from "next";
 import {Server as SocketIOServer} from "socket.io"
 import {Server,Member,Profile} from "@prisma/client"
 export type ServerWithMembersWithProfiles = Server & {
-    member:(Member & {profile:Profile})[];
+    members:(Member & {profile:Profile})[];
 };
 
 export type NextApiResponseServerIo =NextApiResponse &{
